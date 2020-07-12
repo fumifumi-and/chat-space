255455a181b6f6bca4c01a3e31fd1b4174f48b73
fumifumi-and <66513747+fumifumi-and@users.noreply.github.com>
2020-07-12 18:23:16 +0900

    api_edit

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -61,6 +61,6 @@ $('#new_message').on('submit', function(e){
    .fail(function() {
     alert("メッセージ送信に失敗しました");
     $('.chat-main__message-form__send-btn').attr('disabled', false);
-});
+    });
 })
 });
